7b821c1c5cc72deb6836d6d97e3f0420face1c15
QED0711 <qdizon@gmail.com>
2023-05-22 06:26:18 -0400

    updated package num

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,10 @@
 import React from "react";
 import Spiccato from 'spiccato';
 import { managerID, StateObject } from "spiccato/types";
+import { PathNode } from "spiccato/utils/helpers";
 type spiccatoManagerInstance = Spiccato;
 /**************** HOOK IMPLEMENTATION ****************/
-export declare function useSpiccatoState(spiccatoManager: managerID | spiccatoManagerInstance, dependencies: string[] | string[][]): {
+export declare function useSpiccatoState(spiccatoManager: managerID | spiccatoManagerInstance, dependencies: string[] | string[][] | PathNode[]): {
     state: StateObject;
     manager: Spiccato;
 };
